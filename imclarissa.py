c3c93d6b3d22acca8a1497a0812dabe1ead19687
Aluno <aluno@pc-13.isd.matriz>
2019-08-27 14:23:25 -0300

    segunda versao do exercicio do imc - com input

--- imclarissa.py
+++ imclarissa.py
@@ -1,13 +1,17 @@
+print("programa para calcular IMC")
+alturaFloat = input("digite sua altura (em metros): ")
+pesoFloat = input ("digite seu peso (em kg): ")
 
-alturaFloat = 1.70
-pesoFloat = 65
+imc = float(pesoFloat)/(float(alturaFloat)**2)
 
-imc = pesoFloat/(alturaFloat**2)
-print(imc)
-print("A muito abaixo do peso quando?", imc < 17.0)
-print("B abaixo do peso normal quando?", imc >= 17.0 and imc <= 18.5)
-print("C peso dentro do normal quando?", imc  > 18.5 and imc <= 25.0)
-print("D acima do peso normal quando?", imc > 25.0 and imc <= 30.0)
-print("E muito acima do peso normal quando?", imc > 30.0)
 print(imc)
 
+if imc < 17.0:
+    print("A muito baixo peso!")
+elif imc >= 17.0 and imc <= 18.5:
+    print ("B abaixo do peso normal")
+elif imc  > 18.5 and imc <= 25.0:
+    print("C peso dentro do normal")
+elif imc > 25.0 and imc <= 30.0:
+    print("D acima do peso normal")
+else: imc > 30.0
